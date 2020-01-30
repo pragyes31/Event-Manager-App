9702fa287f33a04d0f9a1ebe809169f264355d8b
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-01-30 12:59:46 +0530

    fetched form elements for DOM manipulation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import "./styles.css";
 
 const createEventManagerApp = function() {
-  const eventApp = document.querySelector("#event-manager-app");
   const eventsList = document.querySelector("#events-list");
   const eventNameInput = document.querySelector("#event-name");
-
+  const titleInput = document.querySelector("#title-input");
+  const descriptionInput = document.querySelector("#description-input");
+  const startDateInput = document.querySelector("#start-date-input");
+  const endDateInput = document.querySelector("#end-date-input");
+  const addEventBtn = document.querySelector("#add-event-button");
   const eventsListObject = [];
   const generateEventItem = () => {
     let eventItemDiv = document.createElement("div");
@@ -26,6 +29,11 @@ const createEventManagerApp = function() {
     eventItemDiv.appendChild(eventEndDateDiv);
   };
   const populateEventItem = () => {};
+  const addEventItemObject = e => {
+    e.preventDefault();
+    console.log("boom");
+  };
+  addEventBtn.addEventListener("click", addEventItemObject);
 };
 
 const eventManagerApp = new createEventManagerApp();
